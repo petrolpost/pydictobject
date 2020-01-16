c4d759d5319734203734d5d29ced1d8758b3b587
pdrharris <pdrharris@yahoo.co.uk>
2020-01-16 14:11:51 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Setup file for dictobject"""
+
+from setuptools import setup
+
+setup(
+    name='dictobject',
+    version='0.1',
+    description='DictObject - a dictionary that is accessible like an object',
+    packages=['dictobject'],
+    author='Peter Harris',
+    author_email='peter.harris@nielsen.com',
+    url='https://github.com/pdrharris/dictobject'
+)
