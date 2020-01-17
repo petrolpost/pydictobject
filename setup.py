8db0f124ed1f9b22c998df87ed0a6f9286fe0588
Peter Harris <peter.harris@nielsen.com>
2020-01-17 13:42:15 +0000

    Update setup.py to include info for PyPI

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='pydictobject',
     version='v1.01',
@@ -9,6 +12,13 @@ setup(
     packages=['pydictobject'],
     author='Peter Harris',
     author_email='pdrharris@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/pdrharris/pydictobject',
-    download_url='https://github.com/pdrharris/pydictobject/archive/v1.0.tar.gz'
+    download_url='https://github.com/pdrharris/pydictobject/archive/1.01.tar.gz',
+        classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
